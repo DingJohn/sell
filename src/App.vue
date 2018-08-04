efeff690e5eeae79943959d35f486dfb1ca96e92
chantaldzl@sina.com <chantaldzl@sina.com>
2018-08-04 22:20:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Footer from './components/Footer'
+
+  export default {
+    name: 'App',
+    components: {Footer}
+  }
+</script>
+
+<style>
+</style>
