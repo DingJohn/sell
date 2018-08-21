eac73c692f106dbfa1e013bdd39e4fc6a1f7848d
chantaldzl@sina.com <chantaldzl@sina.com>
2018-08-21 21:36:06 +0800

    update

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-      I am footer vue
-    </div>
+  <div>
+    <div v-model="mes"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Second"
+  export default {
+    name: "Second",
+    data() {
+      return {
+        mes: 'Hello Vue'
+      }
     }
+  }
 </script>
 
 <style scoped>
