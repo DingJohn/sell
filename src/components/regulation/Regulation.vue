09516a5ae16912b34200c98a15f6707d7b02bb53
chantaldzl@sina.com <chantaldzl@sina.com>
2018-09-14 20:32:14 +0800

    update

--- src/components/regulation/Regulation.vue
+++ src/components/regulation/Regulation.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="regulation">
-    <i v-if="count" class="reduce icon-remove_circle_outline" @click="reduceCount"></i>
-    <span v-if="count" class="count" :count="count">{{ count }}</span>
+    <transition name="move">
+      <div class="reduce" v-show="food.count" @click="reduceCount">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
+    <span v-show="food.count" class="count">{{ food.count }}</span>
     <i class="add icon-add_circle" @click="addCount"></i>
   </div>
 </template>
 
 <script>
+
+  import Vue from 'vue'
+
   export default {
     name: "Regulation",
-    data() {
-      return {
-        count: 0
-      }
+    props: {
+      food: {}
     },
     methods: {
-      reduceCount() {
-        this.count--
+      reduceCount(event) {
+        if (!event._constructed) {
+          return
+        }
+        if (this.food.count) {
+          this.food.count--
+        }
       },
-      addCount() {
-        this.count++
+      addCount(event) {
+        if (!event._constructed) {
+          return
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1)
+        } else {
+          this.food.count++
+        }
       }
     }
   }
@@ -28,8 +45,26 @@
 <style lang="stylus">
 
   .regulation
-    display inline-block
-    .reduce, .add
+    font-size 0
+    .reduce
+      opacity 1
+      vertical-align top
+      display inline-block
+      transform: translate3d(0, 0, 0)
+      .inner
+        font-size 24px
+        line-height 24px
+        transform rotate(0)
+        color rgb(0, 160, 220)
+        transition all 0.4s linear
+      &.move-enter-active, &.move-leave-active
+        transition all 0.4s linear
+      &.move-enter, &.move-leave-active
+        opacity 0
+        transform translate3d(24px, 0, 0)
+        .inner
+          transform rotate(180deg)
+    .add
       font-size 24px
       line-height 24px
       vertical-align top
