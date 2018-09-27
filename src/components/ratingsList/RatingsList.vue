a804b01e428309910814cb679fe485ff42d6378b
chantaldzl@sina.com <chantaldzl@sina.com>
2018-09-27 23:28:40 +0800

    update

--- src/components/ratingsList/RatingsList.vue
+++ src/components/ratingsList/RatingsList.vue
@@ -1,27 +1,11 @@
 <template>
   <div>
-    <div class="good-ratings-label">
-      <div class="all-ratings" @click="selected(2,$event)" :class="{'active': type === 2}">
-        <span class="text">{{ titleDesc.all }}</span>
-        <span class="num">{{ ratings.length }}</span>
-      </div>
-      <div class="recommend-ratings" @click="selected(0,$event)" :class="{'active': type === 0}">
-        <span class="text">{{ titleDesc.positive }}</span>
-        <span class="num">{{ positive.length }}</span>
-      </div>
-      <div class="bad-ratings" @click="selected(1,$event)" :class="{'active': type === 1}">
-        <span class="text">{{ titleDesc.negative }}</span>
-        <span class="num">{{ negative.length }}</span>
-      </div>
-    </div>
-    <div class="select-look-over" @click="checkContent">
-      <i class="icon-check_circle" :class="isChecked"></i>
-      <span class="select-desc">只看有内容的评价</span>
-    </div>
+    <v-ratingsSelect :ratings="ratings" :titleDesc="titleDesc" @checkText="getCheckText"
+                     @selectType="getType"></v-ratingsSelect>
     <div class="cut-line"></div>
     <div class="ratings">
-      <ul>
-        <li class="rating-li" v-for="rating in ratings">
+      <ul v-show="ratings && ratings.length">
+        <li v-show="needShow(rating.rateType,rating.text)" class="rating-li" v-for="rating in ratings">
           <div class="time-user">
             <span class="rating-time">{{ rating.rateTime | formatDate}} </span>
             <div class="user-icon">
@@ -35,6 +19,7 @@
           </div>
         </li>
       </ul>
+      <div class="no-rating" v-show="!ratings || !ratings.length">暂无评价</div>
     </div>
   </div>
 </template>
@@ -42,6 +27,7 @@
 <script>
 
   import {formatDate} from '../../common/js/date'
+  import RatingsSelect from '../ratingsSelect/RatingsSelect'
 
   /*赞扬的评价*/
   const POSITIVE = 0
@@ -58,49 +44,35 @@
         default() {
           return []
         }
-      },
-      titleDesc: {
-        default() {
-          return {
-            all: '全部',
-            positive: '满意',
-            negative: '不满意'
-          }
-        }
       }
     },
     data() {
       return {
-        type: 2,
-        checkText: false
+        type: ALL,
+        checkText: false,
+        titleDesc: {
+          all: '全部',
+          positive: '满意',
+          negative: '不满意'
+        }
       }
     },
     methods: {
-      checkContent() {
-        this.checkText = !this.checkText
-      },
-      selected(type, event) {
-        if (!event._constructed) {
-          return
+      needShow(type, text) {
+        if (!text && this.checkText) {
+          return false
         }
+        if (this.type === ALL) {
+          return true
+        } else {
+          return type === this.type
+        }
+      },
+      getType(type) {
         this.type = type
-      }
-    },
-    computed: {
-      isChecked() {
-        if (this.checkText) {
-          return 'current'
-        }
       },
-      positive() {
-        return this.ratings.filter((rating) => {
-          return rating.rateType === POSITIVE
-        })
-      },
-      negative() {
-        return this.ratings.filter((rating) => {
-          return rating.rateType === NEGATIVE
-        })
+      getCheckText(checkText) {
+        this.checkText = checkText
       }
     },
     filters: {
@@ -108,6 +80,9 @@
         let date = new Date(time);
         return formatDate(date, 'yyyy-MM-dd hh:mm');
       }
+    },
+    components: {
+      'v-ratingsSelect': RatingsSelect
     }
   }
 </script>
@@ -115,51 +90,6 @@
 <style lang="stylus">
 
   @import "../../common/stylus/mixin.styl"
-
-  .good-ratings-label
-    font-size 0
-    margin 0 18px
-    padding-bottom 18px
-    border-1px(rgba(7, 17, 27, .1))
-    .all-ratings, .recommend-ratings, .bad-ratings
-      padding 8px 12px
-      border-radius 2px
-      display inline-block
-      color rgb(77, 85, 93)
-      .text
-        font-size 12px
-        padding-right 2px
-      .num
-        font-size 10px
-      &.active
-        color rgb(255, 255, 255)
-    .all-ratings
-      background rgba(0, 160, 220, .2)
-      &.active
-        background rgb(0, 160, 220)
-    .recommend-ratings
-      margin 0 8px
-      background rgba(0, 160, 220, .2)
-      &.active
-        background rgb(0, 160, 220)
-    .bad-ratings
-      background rgba(77, 85, 93, .2)
-      &.active
-        background rgb(77, 85, 93)
-
-  .select-look-over
-    padding 12px 18px
-    line-height 24px
-    .icon-check_circle
-      font-size 24px
-      vertical-align top
-      color rgb(147, 153, 159)
-      &.current
-        color rgb(0, 200, 80)
-    .select-desc
-      font-size 12px
-      vertical-align top
-      color rgb(147, 153, 159)
 
   .cut-line
     width 100%
@@ -209,5 +139,9 @@
           line-height 16px
           display inline-block
           color rgb(7, 17, 27)
+    .no-rating
+      padding: 16px 0
+      font-size: 12px
+      color: rgb(147, 153, 159)
 
 </style>
