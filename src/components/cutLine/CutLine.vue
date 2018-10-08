3553eb300203f3a1d3428eee9b487def56e3bcd7
chantaldzl@sina.com <chantaldzl@sina.com>
2018-10-08 22:42:29 +0800

    first commit

---
+++ src/components/cutLine/CutLine.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="isolation-belt"></div>
+</template>
+
+<script>
+  export default {
+    name: "CutLine"
+  }
+</script>
+
+<style lang="stylus">
+
+  .isolation-belt
+    width 100%
+    height 16px
+    background-color #f3f5f7
+    border-top 1px solid rgba(7, 17, 27, .1)
+    border-bottom 1px solid rgba(7, 17, 27, .1)
+
+</style>
